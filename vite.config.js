75ccba2718eb0fd71865826202d0554a390c0663
Deepu8595 <raod29216@gmail.com>
2024-12-09 18:38:44 +0530

    changes related to the package,vite.config.ts

--- vite.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    build: {
-      outDir: 'build' // If you changed it to 'build', update your deploy script accordingly
-    }
-  };
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://HIMANSHI70489.github.io/portfolio_himanshi',
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react', 'react', 'react-dom'],
   },
+  build: {
+    outDir: 'dist', // Custom output directory
+  },
+
 });
